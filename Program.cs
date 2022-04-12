69670ccd278d3b4e6aee7b7ee15583ba781775a8
MaximSokol <maximsokol2288@gmail.com>
2022-04-12 23:00:45 +0300

    Some Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeStruct str = new SomeStruct("SX-280", "SokolovFactory", 299.79);
+            str.ShowDate();
+
+        }
+    }
+}
---
+++ SomeStruct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    struct SomeStruct
+    {
+        string model;
+        string manufacturer;
+        double price;
+        //-------------------
+        
+        public SomeStruct(string model, string manufacturer, double price)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+        //----------------------------------------------------------------------
+
+        public void ShowDate()
+        {
+            Console.WriteLine($"Model:\t{this.model}\n" +
+                $"Manufacturer:\t{this.manufacturer}\n" +
+                $"Price:\t{this.price}\n");
+        }
+        //---------------------------
+    }
+}
