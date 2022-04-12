3303e8c6483b37f93b7804db92c9b2b5b57ed239
MaximSokol <maximsokol2288@gmail.com>
2022-04-13 00:53:36 +0300

    The third commit

--- Program.cs
+++ Program.cs
@@ -10,8 +10,33 @@ namespace Struct
     {
         static void Main(string[] args)
         {
-            SomeStruct str = new SomeStruct("SX-280", "SokolovFactory", 299.79);
-            str.ShowDate();
+            // The additional task
+
+            //SomeStruct str = new SomeStruct("SX-280", "SokolovFactory", 299.79);
+            //str.ShowDate();
+
+            // Tadk #1
+
+            var obj = new Train();
+
+            var arr = new Train[2];
+
+            obj.FillTheArray(arr);
+            obj.ShowDate(arr);
+
+            obj.Sort(arr);
+            obj.ShowDate(arr);
+
+            obj.SearchTheTrain(arr, 1);
+            obj.ShowDate(arr);
+
+
+
+
+            // Task #2
+
+
+
 
         }
     }
--- Train.cs
+++ Train.cs
@@ -6,7 +6,100 @@ using System.Threading.Tasks;
 
 namespace Struct
 {
-    class Train
+    struct Train
     {
+        public Train[] arr;
+
+        string theNameOfTheDestination;
+        int theNumberOfTheTrain;
+        int theTimeOfLeaving;
+        //----------------------------------
+
+        public Train(string name, int number, int leaving)
+        {
+            this.theNameOfTheDestination = name;
+            this.theNumberOfTheTrain = number;
+            this.theTimeOfLeaving = leaving;
+
+            this.arr = new Train[8];
+        }
+        //---------------------------------------------------
+
+        public string NameOfDestination
+        {
+            get { return theNameOfTheDestination; }
+        }
+
+        public int NumberOfTrain
+        {
+            get { return theNumberOfTheTrain; }
+        }
+
+        public int TimeOFLeaving
+        {
+            get { return theTimeOfLeaving; }
+        }
+        //---------------------------------------------------
+
+        public void FillTheArray(Train []arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter the name ->\t");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter the number ->\t");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("The time of leaving ->\t");
+                int leaving = int.Parse(Console.ReadLine());
+
+                arr[i] = new Train(name, number, leaving);
+            }
+        }
+        //---------------------------------------------------
+
+        public void Sort(Train[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for(int j = 0; j < arr.Length; j++)
+                {
+                    if(arr[i].NumberOfTrain >= arr[j].NumberOfTrain)
+                    {
+                        Train obj = arr[i];
+                        arr[i] = arr[j];
+                        obj = arr[j];
+                    }
+                }
+            }
+        }
+        //---------------------------------------------------
+
+        public void SearchTheTrain(Train[] arr, int poisk)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i].NumberOfTrain == poisk)
+                {
+                    Console.WriteLine($"The number of the train:\t{arr[i].theNumberOfTheTrain}\n" +
+                        $"The way of the destination:\t{arr[i].theNameOfTheDestination}\n" +
+                        $"The time of leaving:\t{arr[i].theTimeOfLeaving}\n");
+                }
+            }
+        }
+        //---------------------------------------------------
+
+        public void ShowDate(Train[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("{0} {1} {2}", 
+                    arr[i].NumberOfTrain,
+                    arr[i].NameOfDestination, 
+                    arr[i].TimeOFLeaving);
+            }
+        }
+        //---------------------------------------------------
     }
 }
